e11650f716ddd58c4ff3bc259ab79995bd66eef7
asthaj2710 <astha2710@gmail.com>
2024-05-24 19:41:56 +0530

    TodoList completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ let taskList=[];
 let inputTodo=document.querySelector("#inputTodo");
 function addTodo(){
     let value=inputTodo.value;
-    taskList.push(value);
+    // taskList.push(value);
+    taskList.unshift(value);
    inputTodo.value='';
    displayList();
 }
@@ -15,7 +16,7 @@ function displayList(){
         newList+=`
         <li class="listitem">
                     <span>${taskList[i]}</span>
-                    <span class="deletebtn">&#x1f5d1;</span>
+                    <span class="deletebtn" onclick="taskList.splice(${i},1); displayList();">&#x1f5d1;</span>
         </li>
         `
     }
